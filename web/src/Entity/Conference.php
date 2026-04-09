cb3fcaf26b21929c72c6c7b59d8f7b3fef0d9996
Sebastien CHOTARD <sebastien.chotard@inetum.com>
2026-04-09 09:12:02 +0200

    chore(project): install symfony webapp project

---
+++ web/src/Entity/Conference.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: ConferenceRepository::class)]
+class Conference
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $isAccessible = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $prerequisites = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endAt = null;
+
+    /**
+     * @var Collection<int, Volunteering>
+     */
+    #[ORM\OneToMany(targetEntity: Volunteering::class, mappedBy: 'conference', orphanRemoval: true)]
+    private Collection $volunteerings;
+
+    /**
+     * @var Collection<int, Organization>
+     */
+    #[ORM\ManyToMany(targetEntity: Organization::class, mappedBy: 'conferences')]
+    private Collection $organizations;
+
+    public function __construct()
+    {
+        $this->volunteerings = new ArrayCollection();
+        $this->organizations = new ArrayCollection();
+    }
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isAccessible(): ?bool
+    {
+        return $this->isAccessible;
+    }
+
+    public function setIsAccessible(bool $isAccessible): static
+    {
+        $this->isAccessible = $isAccessible;
+
+        return $this;
+    }
+
+    public function getPrerequisites(): ?string
+    {
+        return $this->prerequisites;
+    }
+
+    public function setPrerequisites(?string $prerequisites): static
+    {
+        $this->prerequisites = $prerequisites;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeImmutable $startAt): static
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(\DateTimeImmutable $endAt): static
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Volunteering>
+     */
+    public function getVolunteerings(): Collection
+    {
+        return $this->volunteerings;
+    }
+
+    public function addVolunteering(Volunteering $volunteering): static
+    {
+        if (!$this->volunteerings->contains($volunteering)) {
+            $this->volunteerings->add($volunteering);
+            $volunteering->setConference($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVolunteering(Volunteering $volunteering): static
+    {
+        if ($this->volunteerings->removeElement($volunteering)) {
+            // set the owning side to null (unless already changed)
+            if ($volunteering->getConference() === $this) {
+                $volunteering->setConference(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Organization>
+     */
+    public function getOrganizations(): Collection
+    {
+        return $this->organizations;
+    }
+
+    public function addOrganization(Organization $organization): static
+    {
+        if (!$this->organizations->contains($organization)) {
+            $this->organizations->add($organization);
+            $organization->addConference($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrganization(Organization $organization): static
+    {
+        if ($this->organizations->removeElement($organization)) {
+            $organization->removeConference($this);
+        }
+
+        return $this;
+    }
+}
